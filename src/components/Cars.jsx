4df531c50f949b9f97b38122c84ac876894cd0f4
Veronika <veronika.pilipenko@gmail.com>
2022-01-20 18:47:34 -0500

    parsing csv

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,9 +1,27 @@
-import React from 'react'; 
+import React, {useState, useEffect} from 'react'; 
+import { csv } from 'd3';
 
 const Cars = () => {
+
+    const [car, setCar] = useState('')
+
+    useEffect(() => {
+        csv('data.csv').then(data=> { // returns a promise
+            // setCar(data)
+            console.log(data)
+        })
+    }, [])
+
+    // const url = 'data.csv'
+
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+
+
     return (
         <div className="cars-wrapper">
-            <p>hello car stuff will go here</p>
+            <p>{car}</p>
         </div>
     )
 }
