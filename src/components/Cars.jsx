82eb096c277d2f807be4bfaa04ab3e64681f766d
Veronika <veronika.pilipenko@gmail.com>
2022-01-20 19:53:11 -0500

    parsing;

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,27 +1,39 @@
 import React, {useState, useEffect} from 'react'; 
 import { csv } from 'd3';
+import Papa from 'papaparse'
 
 const Cars = () => {
+
+    // const [rows, setRows] = useState('')
+
+        // useEffect(() => {
+        //     async function getData() {
+        //         const response = await fetch('data.csv')
+        //         const reader = response.body.getReader()
+        //         const result = await reader.read() // raw array
+        //         const decoder = new TextDecoder('utf-8')
+        //         const csv = decoder.decode(result.value) // the csv text
+        //         const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
+        //         const rows = results.data // array of objects
+        //         setRows(console.log(rows))
+        //     }
+        //     getData()
+        // }, []) // [] means just do this once, after initial render
+    
 
     const [car, setCar] = useState('')
 
     useEffect(() => {
         csv('data.csv').then(data=> { // returns a promise
-            // setCar(data)
+            setCar(data)
             console.log(data)
         })
     }, [])
 
-    // const url = 'data.csv'
-
-    // fetch(url)
-    //     .then(response => response.json())
-    //     .then(data => console.log(data))
-
-
+    
     return (
         <div className="cars-wrapper">
-            <p>{car}</p>
+            {/* {rows} */}
         </div>
     )
 }
